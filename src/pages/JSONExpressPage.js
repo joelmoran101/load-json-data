6546945e49eb3547b23b808a2c79e87b6bd2dada
Joel.M <joel.moran@students.beaminstitute.org>
2025-11-30 15:27:17 +0100

    fix: updated JSONExpressPage.js to hide the login form temporarily. Use the otp-login branch if login and authentication is preferred.

--- src/pages/JSONExpressPage.js
+++ src/pages/JSONExpressPage.js
@@ -1,16 +1,16 @@
 // src/pages/JSONExpressPage.js
 import React from 'react';
 import { DataContainer } from '../DataContainer';
-import AuthTest from '../components/AuthTest';
+// import AuthTest from '../components/AuthTest'; // Temporarily hidden
 
 const JSONExpressPage = () => {
   return (
     <div className="json-express-page">
       <div className="page-header">
         <h1>Plotly JSON Express Financial Data Dashboard</h1>
-        <p>This App Displays Different Plotly JSON Chart Visualizations of the Financial Data of diverse Companies that are stored in different databases and are accessed via corresponding Backend APIs. The dynamics of highly liquid assets or Current Cash Position(CCP) and some Long-Term Debts and Liabilities (LTD) for several companies over a number of years. You can quickly identify trends and assess the financial stability of the companies. </p>
+        <p>This Demo App Displays Different Plotly JSON Chart Visualizations of the Financial Data of diverse Companies that are stored in different databases and are accessed via corresponding Backend APIs. <strong><em>No Login or Authentication is required.</em></strong> The dynamics of highly liquid assets or Current Cash Position(CCP) and some Long-Term Debts and Liabilities (LTD) for several companies over a number of years. You can quickly identify trends and assess the financial stability of the companies. </p>
       </div>
-      <AuthTest />
+      {/* <AuthTest /> */} {/* Temporarily hidden */}
       <DataContainer />
     </div>
   );
